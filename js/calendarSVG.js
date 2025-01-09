34d754d8739c65e3a87366838cbc66c3e50183ae
micr0dust <wuilliamolgo104286@gmail.com>
2025-01-09 23:02:08 +0800

    first commit

---
+++ js/calendarSVG.js
@@ -0,0 +1,101 @@
+function createSVGElement(data, vaildRow) {
+    for(let i = 0; i < vaildRow.length; i++){
+        if(vaildRow[i] === 1) break;
+        if(i === vaildRow.length - 1){
+            document.getElementById('calendarImage').classList.add('d-none');
+            document.getElementById('copy').classList.add('d-none');
+            return;
+        }
+    }
+    document.getElementById('calendarImage').classList.remove('d-none');
+    document.getElementById('copy').classList.remove('d-none');
+    const svgNS = "http://www.w3.org/2000/svg";
+    const days = ["", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"];
+    const startTime = 8;
+    const endTime = 24;
+    const interval = 15; // 每15分鐘一單位
+    const col_width = 70; // 每一列的寬度
+    const row_height = 20; // 每一行的高度
+
+    let svg_height = 50;
+    for(let i = 0; i < vaildRow.length; i++){
+        if(vaildRow[i] != 0) svg_height += row_height;
+    }
+
+    // 創建SVG元素
+    const svg = document.createElementNS(svgNS, 'svg');
+    svg.setAttribute('width', 700);
+    svg.setAttribute('height', svg_height);
+    svg.setAttribute('xmlns', svgNS);
+
+    // 生成表頭
+    days.forEach((day, index) => {
+        const text = document.createElementNS(svgNS, 'text');
+        text.setAttribute('x', 60 + col_width * index);
+        text.setAttribute('y', row_height);
+        text.textContent = day;
+        svg.appendChild(text);
+    });
+
+    // 生成時間欄
+    let offset = 0;
+    for (let hour = startTime; hour < endTime; hour++) {
+        for (let minute = 0; minute < 60; minute += interval) {
+            const timeText = document.createElementNS(svgNS, 'text');
+            const idx = ((hour - startTime) * 4 + minute / interval);
+            if(vaildRow[idx] === 0){
+                offset++;
+                continue;
+            }
+            timeText.setAttribute('x', 70);
+            timeText.setAttribute('y', 35 + (idx-offset) * row_height);
+            timeText.textContent = `${String(hour).padStart(2, '0')}:${String(minute).padStart(2, '0')}`;
+            svg.appendChild(timeText);
+
+            // 生成每個時間單位的格子
+            for (let dayIndex = 1; dayIndex < days.length; dayIndex++) {
+                const rect = document.createElementNS(svgNS, 'rect');
+                const idx = ((hour - startTime) * 4 + minute / interval);
+                rect.setAttribute('x', 50 + col_width * dayIndex);
+                rect.setAttribute('y', 30 + (idx-offset) * row_height);
+                rect.setAttribute('width', col_width);
+                rect.setAttribute('height', row_height);
+                rect.setAttribute('stroke', 'gray');
+                if(data[dayIndex - 1][idx] === 1){
+                    rect.setAttribute('fill', '#00bfff');
+                }else if(data[dayIndex - 1][idx] === -1){
+                    rect.setAttribute('fill', '#E7E9EB');
+                }
+                else rect.setAttribute('fill', 'white');
+                svg.appendChild(rect);
+            }
+        }
+    }
+
+    // 將SVG轉換為Data URI格式
+    const svgData = new XMLSerializer().serializeToString(svg);
+    const svgBlob = new Blob([svgData], { type: 'image/svg+xml;charset=utf-8' });
+    const url = URL.createObjectURL(svgBlob);
+
+    // 將Data URI設置為img標籤的src屬性
+    document.getElementById('calendarImage').src = url;
+}
+
+function copyImage() {
+    const img = document.getElementById('calendarImage');
+    const canvas = document.createElement('canvas');
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+    canvas.toBlob(blob => {
+        const item = new ClipboardItem({ 'image/png': blob });
+        navigator.clipboard.write([item]);
+        Swal.fire({
+            title: "圖片已複製",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 800
+        });
+    });
+}
---
+++ js/index.js
@@ -0,0 +1,189 @@
+const timeSlotTemplate = document.getElementById('timeSlotTemplate').content;
+const INTERVAL = 15;
+let timeSlotTable = [];
+let data = [];
+let cnt = 0;
+init();
+
+function init() {
+    let counter = 0;
+    for (let i = 8 * 60; i < 24 * 60; i += INTERVAL) {
+        let hours = Math.floor(i / 60);
+        let minutes = i % 60;
+        timeSlotTable.push(`${(hours < 10 ? '0' : '') + hours}:${(minutes < 10 ? '0' : '') + minutes}`);
+        counter++;
+    }
+    timeSlotTable.push(`24:00`);
+}
+
+function addTimeSlot() {
+    // can be beforebegin, afterbegin, beforeend or afterend
+    const element = document.getElementById('main');
+    const clone = document.importNode(timeSlotTemplate, true);
+    setTimeOptions(clone, 'startTime');
+    setTimeOptions(clone, 'endTime');
+    const idlist = ['available','normal', 'weekend', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+    for (let i = 0; i < idlist.length; i++) {
+        let uniqueId = `${idlist[i]}${cnt}`;
+        clone.querySelector(`#${idlist[i]}`).setAttribute('id', uniqueId);
+        clone.querySelector(`label[for="${idlist[i]}"]`).setAttribute('for', uniqueId);
+    }
+    element.insertAdjacentElement('afterend', clone.firstElementChild);
+    document.getElementById('addTimeSlot').disabled = true;
+    cnt++;
+}
+
+function setTimeOptions(element, selectId, begin=0) {
+    let select = element.querySelector(`#${selectId}`);
+    const defaultValue = parseInt(select.value);
+    select.innerHTML = '';
+    let counter = begin;
+    for (let i = 8 * 60 + INTERVAL * begin; i < 24 * 60; i += INTERVAL) {
+        let hours = Math.floor(i / 60);
+        let minutes = i % 60;
+        let option = document.createElement('option');
+        option.value = counter;
+        option.text = `${(hours < 10 ? '0' : '') + hours}:${(minutes < 10 ? '0' : '') + minutes}`;
+        select.appendChild(option);
+        counter++;
+    }
+    let option = document.createElement('option');
+    option.value = counter;
+    option.text = `24:00`;
+    select.appendChild(option);
+    if(begin && defaultValue >= begin) select.value = defaultValue;
+}
+
+function handleTimeChange(element) {
+    const startTime = parseInt(element.querySelector(`#startTime`).value);
+    setTimeOptions(element, 'endTime', startTime);
+}
+
+function removeTimeSlot(element) {
+    element.remove();
+    saveToData();
+    if(document.getElementById('save') === null)
+        document.getElementById('addTimeSlot').disabled = false;
+}
+
+function saveTimeSlots(timeSlot) {
+    timeSlot.classList.remove('border-danger');
+    timeSlot.querySelector('#error').classList.add('d-none');
+    const startTime = parseInt(timeSlot.querySelector('#startTime').value);
+    const endTime = parseInt(timeSlot.querySelector('#endTime').value);
+    if (startTime > endTime) return card_error(timeSlot, '開始時間必須早於結束時間');
+    if (startTime == endTime) return card_error(timeSlot, '你時間設這樣有什麼意義？');
+    const daysElement = timeSlot.querySelector('#days');
+    const inputElements = daysElement.querySelectorAll('input');
+    let flag = 0;
+    for (let i = 0; i < inputElements.length; i++)
+        flag |= inputElements[i].checked;
+    if (!flag) return card_error(timeSlot, '請選擇欲套用的日期');
+    const checkboxes = timeSlot.querySelectorAll('input');
+    for (let i = 0; i < checkboxes.length; i++)
+        checkboxes[i].disabled = true;
+    timeSlot.querySelector('#startTime').disabled = true;
+    timeSlot.querySelector('#endTime').disabled = true;
+    timeSlot.querySelector('#save').remove();
+    timeSlot.querySelector('#title').contentEditable = false;
+    saveToData();
+    document.getElementById('addTimeSlot').disabled = false;
+}
+
+function card_error(timeSlot, msg) {
+    timeSlot.classList.add('border-danger');
+    const error = timeSlot.querySelector('#error');
+    error.textContent = msg;
+    error.classList.remove('d-none');
+}
+
+function moveUp(card) {
+    const prevCard = card.previousElementSibling;
+    if (prevCard && prevCard.tagName.toLowerCase() == 'div') card.parentNode.insertBefore(card, prevCard);
+    saveToData();
+}
+
+function moveDown(card) {
+    const nextCard = card.nextElementSibling;
+    if (nextCard && nextCard.tagName.toLowerCase() == 'div') card.parentNode.insertBefore(nextCard, card);
+    saveToData();
+}
+
+function saveToData() {
+    const segments = Array.from(document.querySelectorAll('.card')).reverse();
+    let tmp_data = [];
+    for (let i = 0; i < segments.length; i++) {
+        let days = segments[i].querySelector('#days').querySelectorAll('input');
+        let daySelected = 0;
+        for (let j = 0; j < days.length; j++)
+            daySelected |= days[j].checked << j;
+        tmp_data.push({
+            title: segments[i].querySelector('#title').textContent,
+            startTime: parseInt(segments[i].querySelector('#startTime').value),
+            endTime: parseInt(segments[i].querySelector('#endTime').value),
+            days: intToBase64(daySelected),
+            mode: segments[i].querySelector('[name="available"]').checked? -1:1
+        });
+    }
+    data = tmp_data;
+    createSVGElement(segmentsToTable(data), vaildRow(data));
+}
+
+function segmentsToTable(data){
+    let table = new Array(7).fill(0).map(() => new Array((24-8)*60/15).fill(0));
+    for(let i = 0; i < data.length; i++){
+        let days = base64ToInt(data[i].days);
+        for(let j = 0; j < 7; j++){
+            if(days & (1 << j)){
+                for(let k = data[i].startTime; k < data[i].endTime; k++)
+                    table[j][k] = data[i].mode;
+            }
+        }
+    }
+    return table;
+}
+
+function vaildRow(data){
+    let table = new Array((24-8)*60/15).fill(0);
+    for(let i = 0; i < data.length; i++)
+        table[data[i].startTime] = table[data[i].endTime] = 1;
+    return table;
+}
+
+function checkboxNormal(element) {
+    if(element.disabled) return;
+    let checkboxes = Array.from(
+        element.closest('.card').querySelector('#days').querySelectorAll('input')
+    ).slice(0, 5);
+    if(element.checked) checkboxes.forEach(checkbox => checkbox.checked = true);
+    else checkboxes.forEach(checkbox => checkbox.checked = false);
+}
+
+function checkboxWeekend(element) {
+    if(element.disabled) return;
+    let checkboxes = Array.from(
+        element.closest('.card').querySelector('#days').querySelectorAll('input')
+    ).slice(5);
+    if(element.checked) checkboxes.forEach(checkbox => checkbox.checked = true);
+    else checkboxes.forEach(checkbox => checkbox.checked = false);
+}
+
+function checkNormal(element) {
+    if(element.disabled) return;
+    if(!element.checked) element.closest('.card').querySelector('[name="normal"]').checked = false;
+}
+
+function checkWeekend(element) {
+    if(element.disabled) return;
+    if(!element.checked) element.closest('.card').querySelector('[name="weekend"]').checked = false;
+}
+
+function availableSwitch(element) {
+    if(element.disabled) return;
+    const title = element.closest('.card').querySelector('#title');
+    if(element.checked && title.textContent === '有空的時段'){
+        title.textContent = '沒空的時段';
+    }else if(!element.checked && title.textContent === '沒空的時段'){
+        title.textContent = '有空的時段';
+    }
+}
